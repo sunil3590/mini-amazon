57965942689d97f12f9fed806acc81176ecd85c3
Sunil D S <sunil@zabbed.com>
2018-04-06 13:03:38 +0530

    more changes

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # mini-amazon
 for the folks at RNSIT
+
+```bash
+git clone https://github.com/sunil3590/mini-amazon.git
+cd mini-amazon
+pip install -r requirements.txt
+python run.py
+```
--- run.py
+++ run.py
@@ -2,14 +2,19 @@ from flask import Flask
 app = Flask('mini-amazon')
 
 
+@app.route('/health', methods=['GET'])
+def health():
+    return 'healthy'
+
+
 @app.route('/')
 def index():
     return "<h1>Welcome to Mini Amazon</h1>"
 
 
-@app.route('/products/<name>')
+@app.route('/say-hello/<name>')
 def add_product(name):
-    return '<h1>This is, {0}!</h1>'.format(name)
+    return '<h1>Hello, {0}!</h1>'.format(name)
 
 
 if __name__ == '__main__':
